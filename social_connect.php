e8f88b74b66aa2a012cb5a25c2881c4922f978d0
Ashwin Phatak <ashwinpphatak@gmail.com>
2010-11-16 15:49:19 +0530

    settings for twitter

--- social_connect.php
+++ social_connect.php
@@ -69,6 +69,9 @@ function sc_register_social_connect_settings()
 {
 	register_setting( 'social-connect-settings-group', 'social_connect_facebook_api_key' );
 	register_setting( 'social-connect-settings-group', 'social_connect_facebook_secret_key' );
+  register_setting( 'social-connect-settings-group', 'social_connect_twitter_enabled');
+  register_setting( 'social-connect-settings-group', 'social_connect_twitter_consumer_key');
+  register_setting( 'social-connect-settings-group', 'social_connect_twitter_consumer_secret');
 }
 
 
@@ -80,15 +83,41 @@ function sc_render_social_connect_settings()
 
 <form method="post" action="options.php">
     <?php settings_fields( 'social-connect-settings-group' ); ?>
+
+    <h3>Facebook Settings</h3>
     <table class="form-table">
         <tr valign="top">
-        <th scope="row">Facebook API Key</th>
+        <th scope="row">API Key</th>
         <td><input type="text" name="social_connect_facebook_api_key" value="<?php echo get_option('social_connect_facebook_api_key'); ?>" /></td>
         </tr>
 
         <tr valign="top">
-        <th scope="row">Facebook Secret Key</th>
+        <th scope="row">Secret Key</th>
         <td><input type="text" name="social_connect_facebook_secret_key" value="<?php echo get_option('social_connect_facebook_secret_key'); ?>" /></td>
+        </tr>
+    </table>
+
+    <br/><br/>
+
+    <h3>Twitter Settings</h3>
+    <table class="form-table">
+        <tr valign="top">
+        <th scope="row">Enable?</th>
+        <td>
+          <input type="checkbox" name="social_connect_twitter_enabled" value="1" <?php checked(get_option('social_connect_twitter_enabled'), 1); ?> /><br/>
+          Twitter integration requires the generation of dummy email addresses for authenticating users. <br/>
+          Please check with your domain administrator as this may require changes to your mail server.
+        </td>
+        </tr>
+
+        <tr valign="top">
+        <th scope="row">Consumer Key</th>
+        <td><input type="text" name="social_connect_twitter_consumer_key" value="<?php echo get_option('social_connect_twitter_consumer_key'); ?>" /></td>
+        </tr>
+
+        <tr valign="top">
+        <th scope="row">Consumer Secret</th>
+        <td><input type="text" name="social_connect_twitter_consumer_secret" value="<?php echo get_option('social_connect_twitter_consumer_secret'); ?>" /></td>
         </tr>
     </table>
 
