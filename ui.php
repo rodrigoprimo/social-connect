017aa543a99b3b2ecc9916e634dcbe232b4a2b33
Brent Shepherd <thenbrent@gmail.com>
2011-04-25 16:38:55 +0700

    Fixing strict warnings from widget. Removing priorities on hooks. Cleaning parameters for social-connect render login form function.

--- ui.php
+++ ui.php
@@ -1,7 +1,14 @@
 <?php
 
-function sc_render_login_form_social_connect($no_args, $display_label=true) {
-	$images_url = SOCIAL_CONNECT_PLUGIN_URL . '/media/img/';
+function sc_render_login_form_social_connect( $args = NULL ) {
+
+	if( $args == NULL )
+		$display_label = true;
+	elseif ( is_array( $args ) )
+		extract( $args );
+
+	if( !isset( $images_url ) )
+		$images_url = SOCIAL_CONNECT_PLUGIN_URL . '/media/img/';
 
 	$twitter_enabled = get_option('social_connect_twitter_enabled') && get_option('social_connect_twitter_consumer_key') && get_option('social_connect_twitter_consumer_secret');
 	$facebook_enabled = get_option('social_connect_facebook_enabled', 1) && get_option('social_connect_facebook_api_key') && get_option('social_connect_facebook_secret_key');
@@ -9,7 +16,6 @@ function sc_render_login_form_social_connect($no_args, $display_label=true) {
 	$yahoo_enabled = get_option('social_connect_yahoo_enabled', 1);
 	$wordpress_enabled = get_option('social_connect_wordpress_enabled', 1);
 	?>
-
 	<div id="social_connect_ui">
 		<?php if($display_label!==false) : ?>
 		<div style="margin-bottom: 3px;"><label><?php _e( 'Connect with', 'social_connect' ); ?>:</label></div>
@@ -37,7 +43,6 @@ function sc_render_login_form_social_connect($no_args, $display_label=true) {
 		$social_connect_user_name = isset($_COOKIE['social_connect_current_name']) ? $_COOKIE['social_connect_current_name'] : '';
 		$social_connect_wordpress_blog_url = isset($_COOKIE['social_connect_wordpress_blog_url']) ? $_COOKIE['social_connect_wordpress_blog_url'] : '';
 		$social_connect_openid_url = isset($_COOKIE['social_connect_openid_url']) ? $_COOKIE['social_connect_openid_url'] : '';
-
 
 		if($social_connect_wordpress_blog_url == '') {
 			$social_connect_wordpress_blog_name = '';
@@ -94,9 +99,9 @@ function sc_render_login_form_social_connect($no_args, $display_label=true) {
 	</div> <!-- End of social_connect_ui div -->
 <?php
 }
-add_action('login_form', 'sc_render_login_form_social_connect', null, 2);
-add_action('register_form', 'sc_render_login_form_social_connect', null, 2);
-add_action('after_signup_form', 'sc_render_login_form_social_connect', null, 2);
+add_action('login_form', 'sc_render_login_form_social_connect' );
+add_action('register_form', 'sc_render_login_form_social_connect' );
+add_action('after_signup_form', 'sc_render_login_form_social_connect' );
 
 
 
@@ -164,26 +169,37 @@ class SocialConnectWidget extends WP_Widget {
 			'social_connect', //unique id
 			'Social Connect', //title displayed at admin panel
 			//Additional parameters
-			array('description' => __('Adds functionality to connect with Facebook, Twitter, Yahoo, Google or Wordpress.com account', 'social_connect'))
+			array('description' => __('Login or register with Facebook, Twitter, Yahoo, Google or a Wordpress.com account', 'social_connect'))
 		);
 	}
 
 	/** This is rendered widget content */
 	function widget($args, $instance) {
 		extract( $args );
-		$title = apply_filters('widget_title', $instance['title']);
 		echo $before_widget;
-		if ( $title ) echo $before_title . $title . $after_title;
-		echo $instance['before_widget_content'];
-		sc_render_login_form_social_connect(null, false);
-		echo $instance['after_widget_content'];
+
+		if( !empty( $instance['title'] ) ){
+			$title = apply_filters('widget_title', $instance[ 'title' ] );
+			echo $before_title . $title . $after_title;
+		}
+
+		if( !empty( $instance[ 'before_widget_content' ] ) ){
+			echo '<p>' . $instance['before_widget_content'] . '</p>';
+		}
+
+		sc_render_login_form_social_connect( array( 'display_label' => false ) );
+
+		if( !empty( $instance[ 'after_widget_content' ] ) ){
+			echo '<p>' . $instance['after_widget_content'] . '</p>';
+		}
+
 		echo $after_widget;
 	}
 
 	/** Everything which should happen when user edit widget at admin panel */
 	function update($new_instance, $old_instance) {
 		$instance = $old_instance;
-		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['title'] = strip_tags( $new_instance['title'] );
 		$instance['before_widget_content'] = $new_instance['before_widget_content'];
 		$instance['after_widget_content'] = $new_instance['after_widget_content'];
 
@@ -192,22 +208,22 @@ class SocialConnectWidget extends WP_Widget {
 
 	/** Widget edit form at admin panel */
 	function form($instance) {
-		$title = esc_attr($instance['title']);
-		$before_widget_content = esc_attr($instance['before_widget_content']);
-		$after_widget_content = esc_attr($instance['after_widget_content']);
+		/* Set up default widget settings. */
+		$defaults = array( 'title' => '', 'before_widget_content' => '', 'after_widget_content' => '' );
+		$instance = wp_parse_args( (array)$instance, $defaults );
 		?>
 		<p>
 			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'social_connect'); ?></label>
-			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $instance['title']; ?>" />
 			<label for="<?php echo $this->get_field_id('before_widget_content'); ?>"><?php _e('Before widget content:', 'social_connect'); ?></label>
-			<input class="widefat" id="<?php echo $this->get_field_id('before_widget_content'); ?>" name="<?php echo $this->get_field_name('before_widget_content'); ?>" type="text" value="<?php echo $before_widget_content; ?>" />
+			<input class="widefat" id="<?php echo $this->get_field_id('before_widget_content'); ?>" name="<?php echo $this->get_field_name('before_widget_content'); ?>" type="text" value="<?php echo $instance['before_widget_content']; ?>" />
 			<label for="<?php echo $this->get_field_id('after_widget_content'); ?>"><?php _e('After widget content:', 'social_connect'); ?></label>
-			<input class="widefat" id="<?php echo $this->get_field_id('after_widget_content'); ?>" name="<?php echo $this->get_field_name('after_widget_content'); ?>" type="text" value="<?php echo $after_widget_content; ?>" />
+			<input class="widefat" id="<?php echo $this->get_field_id('after_widget_content'); ?>" name="<?php echo $this->get_field_name('after_widget_content'); ?>" type="text" value="<?php echo $instance['after_widget_content']; ?>" />
 		</p>
 		<?php
 	}
 
-} // end of class SocialConnectWidget
+}
 add_action('widgets_init', create_function('', 'return register_widget("SocialConnectWidget");'));
 
 
