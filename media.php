59d445981ec7f5c3230fe4f7f5c6f785d21e3ad8
Rodrigo Primo <rodrigo@hacklab.com.br>
2013-12-16 13:12:00 -0200

    include jquery-ui-dialog only if wordpress login is enabled

--- media.php
+++ media.php
@@ -33,11 +33,18 @@ add_action( 'admin_print_styles', 'sc_add_admin_stylesheets' );
 
 
 function sc_add_javascripts(){
+	$deps = array( 'jquery', 'jquery-ui-core' );
+	$wordpress_enabled = get_option( 'social_connect_wordpress_enabled' );
+	
+	if ( $wordpress_enabled ) {
+		$deps[] = 'jquery-ui-dialog';
+	}
 
 	if( ! wp_script_is( 'social_connect', 'registered' ) )
-		wp_register_script( 'social_connect', SOCIAL_CONNECT_PLUGIN_URL . '/media/js/connect.js', array( 'jquery', 'jquery-ui-core', 'jquery-ui-dialog' ) );
+		wp_register_script( 'social_connect', SOCIAL_CONNECT_PLUGIN_URL . '/media/js/connect.js', $deps );
 
 	wp_enqueue_script( 'social_connect' );
+	wp_localize_script( 'social_connect', 'social_connect_data', array( 'wordpress_enabled' => $wordpress_enabled ) );
 }
 add_action( 'login_head',         'sc_add_javascripts' );
 add_action( 'wp_enqueue_scripts', 'sc_add_javascripts' );
