6b90a169276ea534dc35fd8a206914df7c33ebd3
Rodrigo Primo <rodrigo@hacklab.com.br>
2014-02-10 15:55:34 -0200

    change svn username in deploy script

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ GITPATH="$CURRENTDIR/" # this file should be in the base of your git repository
 # svn config
 SVNPATH="/tmp/$PLUGINSLUG" # path to a temp SVN repo. No trailing slash required and don't add trunk.
 SVNURL="http://plugins.svn.wordpress.org/social-connect/" # Remote SVN repo on wordpress.org, with no trailing slash
-SVNUSER="thenbrent" # your svn username
+SVNUSER="rodrigosprimo" # your svn username
 
 
 # Let's begin...
