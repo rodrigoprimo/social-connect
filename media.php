1c4783695c007a24c9741f0fafe05d2e99302c7f
Rodrigo Primo <rodrigo@hacklab.com.br>
2014-04-19 17:34:34 -0300

    use login_enqueue_scripts instead of login_head

--- media.php
+++ media.php
@@ -13,7 +13,7 @@ function sc_add_stylesheets(){
 		wp_enqueue_style( "wp-jquery-ui-dialog" );
 	}
 }
-add_action( 'login_head', 'sc_add_stylesheets' );
+add_action( 'login_enqueue_scripts', 'sc_add_stylesheets' );
 add_action( 'wp_head', 'sc_add_stylesheets' );
 
 
@@ -45,5 +45,5 @@ function sc_add_javascripts(){
 	wp_enqueue_script( 'social_connect' );
 	wp_localize_script( 'social_connect', 'social_connect_data', array( 'wordpress_enabled' => $wordpress_enabled ) );
 }
-add_action( 'login_head',         'sc_add_javascripts' );
+add_action( 'login_enqueue_scripts', 'sc_add_javascripts' );
 add_action( 'wp_enqueue_scripts', 'sc_add_javascripts' );
